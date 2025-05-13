bc325f2dc406be6b3ae38a61aef2fd2d3e5a54a9
ebarbosa27 <ebarbosa5449@gmail.com>
2025-05-12 23:51:32 -0500

    features to js

---
+++ index.js
@@ -0,0 +1,25 @@
+function convertToCelsius(tempF) {
+  // (°F − 32) × 5/9 = °C
+  return (tempF - 32) * (5 / 9);
+}
+
+function describeTemperature(tempF) {
+  const tempC = tempF;
+
+  if (tempC < 0) {
+    return "very cold";
+  }
+  if (tempC < 20) {
+    return "cold";
+  }
+  if (tempC < 30) {
+    return "warm";
+  }
+  if (tempC < 40) {
+    return "hot";
+  }
+  return "very hot";
+}
+
+const userInput = prompt("What is the temperature? (°F)");
+alert(`Seems like it's ${describeTemperature(userInput)} outside today.`);
